7e04e18fe143b34dfaeff71715a2718dc3344f00
Natasha <nlackas12@gmail.com>
2023-04-16 12:37:10 -0500

    added comments and readme info

--- README.md
+++ README.md
@@ -2,14 +2,19 @@
 
 ## Description
 
-A command-line application that takes inuser input to generate a logo and saves it as a logo.svg file.
+A command-line application that takes inuser input to generate a logo and saves it as a logo.svg file. A user can follow the prompts and a unique logo will be generated.
+
+## Installation
+Install inquier 8.2.4 and jest from your terminal. You can then clone this repository and run from your terminal.
 
 ## Image
 
-![alt text](./Assets/nlewis742.github.io_weather-dash_.png)
+![alt text](./Assets/Capture.PNG)
 
 ## Link
 
 https://github.com/nlewis742/SVG_generator
+
+## Video Link
 
 
--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/Shape");
 
+// prompts for user input
 inquirer
   .prompt([
     {
@@ -51,7 +38,7 @@ inquirer
     },
   ])
   .then((data) => {
-    
+    //promises to create the svg file based on user input
     function createSVG(data) {
     fs.writeFile("logo.svg", data, (err) =>
       err ? console.log(err) : console.log("successfully created logo"))
@@ -73,8 +60,8 @@ inquirer
               break;
             }
             return shape;
-            // console.log(shape);
           };
+          //defines the structure of the svg file
           const svg =
           `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
           
@@ -84,38 +71,4 @@ inquirer
           </svg>`;
           createSVG(svg);
         });
-        // <${Shape()} width="100%" height="100%" fill=${data.shapeColor} />
-        // function logo(text, textColor, shape, shapeColor) {
-        //   this.text = text;
-        //   this.textColor = textColor;
-        //   this.shape = shape;
-        //   this.shapeColor = shapeColor;
-        // }
-
-    
-
-    // const svgPageContent
-
-  // init();
-    
-  
-
-// .then((answers) => {
-//     const svgPageContent = generateSVG(answers);
-
-//     fs.writeFile('logo.svg', svgPageContent, (err) =>
-//     err ? console.log(err) : console.log('Successfully created logo.svg!'));
-//   })
-
-// //I need established objects for text color, shape, selection of shapes and color of shapes for a user to pull from.
-//   this.create = function () {
-//       console.log("created");
-//   }
-// }
-// //Constructor function to create objects containing properties "shapes", "text", "color"
-
-//I need to store the user's input in something
-
-//I need to create a svg file for them to view with their logo
-
-//I have tests in jest for my prompts
+   
